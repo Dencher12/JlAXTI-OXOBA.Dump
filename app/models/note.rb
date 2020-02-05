185fadce54b1c528eb07b84942fd6f56690cf57d
Den <denischerry19@gmail.com>
2020-02-05 09:33:01 +0300

    Невский теперь ссылка и поле body необязательное

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,5 @@
 class Note < ApplicationRecord
   has_many :attachments, as: :attachable
-  validates :title, :body, presence: true
+  validates :title, presence: true
   accepts_nested_attributes_for :attachments
 end
