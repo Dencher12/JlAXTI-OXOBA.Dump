c2594c955f22e004c94657b18802421b55cc7099
Dencher12 <denischerry19@gmail.com>
2020-05-30 12:45:00 +0300

    Изменил порядок отобоажения записей, добавил change log

---
+++ app/controllers/change_log_controller.rb
@@ -0,0 +1,2 @@
+class ChangeLogController < ApplicationController
+end
--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,6 @@ class Note < ApplicationRecord
   has_many :attachments, as: :attachable, dependent: :destroy
   validates :title, presence: true
   accepts_nested_attributes_for :attachments
+
+  default_scope { order(created_at: :desc) } 
 end
